be48865538d651ec6c350804269cd0f17b64198e
mamun2519 <juborajislam46@gmail.com>
2024-12-08 18:54:52 +0600

    added to c progarmming code

--- c-programming/variable.c
+++ c-programming/variable.c
@@ -6,6 +6,9 @@
 int main()
 {
       int myNum = 15;
-      printf("d%", myNum);
+      printf("This is a number %d\n", myNum);
+      int ftNum = 20;
+      int result = myNum + ftNum;
+      printf("This is result %d\n", result);
       return 0;
 }
