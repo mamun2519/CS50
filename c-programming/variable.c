af5731fa92bdfebd91150c6252e209241fd949fc
mamun2519 <juborajislam46@gmail.com>
2024-12-08 18:58:01 +0600

    added to c progarmming code

--- c-programming/variable.c
+++ c-programming/variable.c
@@ -5,10 +5,15 @@
 
 int main()
 {
+      // Number
       int myNum = 15;
       printf("This is a number %d\n", myNum);
       int ftNum = 20;
       int result = myNum + ftNum;
       printf("This is result %d\n", result);
+
+      // cher
+      char myName = "Mohammad Mamun";
+      printf("Hi there\n. My Name Is %c", myName);
       return 0;
 }
